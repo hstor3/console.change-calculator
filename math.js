3b1f73d55c07cb51060723242d01b6ddca289aab
Heather <heather@3iautoshot.com>
2022-04-26 12:15:27 -0400

    loops through change array and logs return amount

--- math.js
+++ math.js
@@ -9,11 +9,20 @@ function calculate(number) {
 
     for (let i = 0; i < dollarArr.length; i++) {
         if (dollars >= dollarArr[i]) {
-          dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
-          console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
-          dollars = (dollars % dollarArr[i])
+            dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
+            console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
+            dollars = (dollars % dollarArr[i])
         }
-      }
+    }
+
+    for (let i = 0; i < changeArr.length; i++) {
+        if (cents >= changeArr[i]) {
+            changeDivisible.push(Math.floor(cents / changeArr[i]))
+            console.log(`$.${changeArr[i]}s - Quantity: ${changeDivisible.slice(-1)}`)
+            cents = (cents % changeArr[i])
+        }
+
+    }
 }
 
 console.log(calculate(12.34))
