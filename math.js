bdfc17aeb20d325fc773cc841ab71fb84b795771
Heather <heather@3iautoshot.com>
2022-04-26 12:14:34 -0400

    loop through dollar array and logs return amount

--- math.js
+++ math.js
@@ -1,6 +1,19 @@
 function calculate(number) {
+    let dollarDivisible = [];
+    let changeDivisible = [];
+    let num = (number + "").split(".");
+    let dollars = num[0];
+    let cents = num[1];
     let dollarArr = [20, 10, 5, 1];
     let changeArr = [25, 10, 5, 1];
+
+    for (let i = 0; i < dollarArr.length; i++) {
+        if (dollars >= dollarArr[i]) {
+          dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
+          console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
+          dollars = (dollars % dollarArr[i])
+        }
+      }
 }
 
 console.log(calculate(12.34))
