b25b70b31de1f5c4a1f2086b97b36921d8df2871
Heather <heather@3iautoshot.com>
2022-04-26 12:18:07 -0400

    inquirer added and ignored

--- math.js
+++ math.js
@@ -1,28 +1,41 @@
-function calculate(number) {
-    let dollarDivisible = [];
-    let changeDivisible = [];
-    let num = (number + "").split(".");
-    let dollars = num[0];
-    let cents = num[1];
-    let dollarArr = [20, 10, 5, 1];
-    let changeArr = [25, 10, 5, 1];
+let inquirer = require('inquirer');
 
-    for (let i = 0; i < dollarArr.length; i++) {
-        if (dollars >= dollarArr[i]) {
+inquirer
+   .prompt([
+      {
+         type: 'number',
+         name: 'number',
+         message: 'Please enter a number amount to calculate change.'
+      }
+   ])
+   .then((number) => {
+      let dollarDivisible = [];
+      let changeDivisible = [];
+      let num = (number.number + "").split(".");
+      let dollars = num[0];
+      let cents = num[1];
+      let dollarArr = [20, 10, 5, 1];
+      let changeArr = [25, 10, 5, 1];
+
+      for (let i = 0; i < dollarArr.length; i++) {
+         if (dollars >= dollarArr[i]) {
             dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
             console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
             dollars = (dollars % dollarArr[i])
-        }
-    }
+         }
+      }
 
-    for (let i = 0; i < changeArr.length; i++) {
-        if (cents >= changeArr[i]) {
+      for (let i = 0; i < changeArr.length; i++) {
+         if (cents >= changeArr[i]) {
             changeDivisible.push(Math.floor(cents / changeArr[i]))
             console.log(`$.${changeArr[i]}s - Quantity: ${changeDivisible.slice(-1)}`)
             cents = (cents % changeArr[i])
-        }
+         }
 
-    }
-}
-
-console.log(calculate(12.34))
+      }
+   })
+   .catch((error) => {
+      if (error) {
+         console.log('An error occurred. Please try again.')
+      }
+   });
