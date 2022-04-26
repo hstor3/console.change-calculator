e6467939b49b091d718582152133fb859d4b717a
Heather <heather@3iautoshot.com>
2022-04-26 12:20:59 -0400

    changed indentation

--- math.js
+++ math.js
@@ -1,41 +1,43 @@
 let inquirer = require('inquirer');
 
 inquirer
-   .prompt([
-      {
-         type: 'number',
-         name: 'number',
-         message: 'Please enter a number amount to calculate change.'
-      }
-   ])
-   .then((number) => {
-      let dollarDivisible = [];
-      let changeDivisible = [];
-      let num = (number.number + "").split(".");
-      let dollars = num[0];
-      let cents = num[1];
-      let dollarArr = [20, 10, 5, 1];
-      let changeArr = [25, 10, 5, 1];
+  .prompt([
+    {
+      type: 'number',
+      name: 'number',
+      message: 'Please enter a number amount to calculate change.'
+    }
+  ])
 
-      for (let i = 0; i < dollarArr.length; i++) {
-         if (dollars >= dollarArr[i]) {
-            dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
-            console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
-            dollars = (dollars % dollarArr[i])
-         }
+  .then((number) => {
+    let dollarDivisible = [];
+    let changeDivisible = [];
+    let num = (number.number + "").split(".");
+    let dollars = num[0];
+    let cents = num[1];
+    let dollarArr = [20, 10, 5, 1];
+    let changeArr = [25, 10, 5, 1];
+
+    for (let i = 0; i < dollarArr.length; i++) {
+      if (dollars >= dollarArr[i]) {
+        dollarDivisible.push(Math.floor(dollars / dollarArr[i]))
+        console.log(`$${dollarArr[i]}s - Quantity: ${dollarDivisible.slice(-1)}`)
+        dollars = (dollars % dollarArr[i])
+      }
+    }
+
+    for (let i = 0; i < changeArr.length; i++) {
+      if (cents >= changeArr[i]) {
+        changeDivisible.push(Math.floor(cents / changeArr[i]))
+        console.log(`$.${changeArr[i]}s - Quantity: ${changeDivisible.slice(-1)}`)
+        cents = (cents % changeArr[i])
       }
 
-      for (let i = 0; i < changeArr.length; i++) {
-         if (cents >= changeArr[i]) {
-            changeDivisible.push(Math.floor(cents / changeArr[i]))
-            console.log(`$.${changeArr[i]}s - Quantity: ${changeDivisible.slice(-1)}`)
-            cents = (cents % changeArr[i])
-         }
+    }
+  })
 
-      }
-   })
-   .catch((error) => {
-      if (error) {
-         console.log('An error occurred. Please try again.')
-      }
-   });
+  .catch((error) => {
+    if (error) {
+      console.log('An error occurred. Please try again.')
+    }
+  });
